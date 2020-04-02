0a876b77ec818a836dfa2d98fbe30d7074df5cec
priyanshisharma <priyanshi417@gmail.com>
2020-04-03 02:55:30 +0530

    just another commit

--- mess/api/views.py
+++ mess/api/views.py
@@ -33,23 +33,25 @@ def create(request,username):
     return Response(serializer.errors, status=400)
 
 
-#@csrf_exempt
-#@api_view(['POST'])
-#@permission_classes((AllowAny,))
-#def login(request):
-#    if request.method == 'POST':
-#        username = request.data.get("username")
-#        password = request.data.get("password")
-#        if username is None or password is None:
-#            return Response({'error': 'Please provide both username and password'},status=HTTP_400_BAD_REQUEST)
-#        user = authenticate(username=username, password=password)
-#        if not user:
-#            return Response({'error': 'Invalid Credentials'},
-#                            status=HTTP_404_NOT_FOUND)
-#        queryset = Message.objects.filter(username__exact=username)
+@csrf_exempt
+@api_view(['POST'])
+@permission_classes((AllowAny,))
+def login(request):
+    if request.method == 'POST':
+        email = request.data.get("email")
+        password = request.data.get("password")
+        username = request.data.get("username")
+
+        if username is None or password is None:
+            return Response({'error': 'Please provide both email and password'},status=HTTP_400_BAD_REQUEST)
+        user = authenticate(email=email, password=password)
+        if not user:
+            return Response({'error': 'Invalid Credentials'},
+                            status=HTTP_404_NOT_FOUND)
+        queryset = Message.objects.filter(username__exact=username)
         #queryset = Message.objects.all()       
-#        serializer = MessageSerializer(queryset,many=True)
-#        return Response(serializer.data)
+        serializer = MessageSerializer(queryset,many=True)
+        return Response(serializer.data)
 '''
 @api_view(['GET'])
 def list(request,username):
